d22fd919377908c2d845aa11dc1f9e6c40bc6d23
romfra <romina.fratoni@gmail.com>
2020-04-15 22:43:07 +0100

    RandomForest

--- EShopPrediction_RF.py
+++ EShopPrediction_RF.py
@@ -19,24 +19,25 @@ print(dataset.head())
 print(dataset.shape)
 print(dataset.info())
 print(dataset.describe())
+print(dataset.head(2))
 
 # Converting Categorical features into Numerical features
-def converter(column):
-    if column == 'TRUE':
-        return 1
-    else:
-        return 0
+#def converter(column):
+#    column.astype(int)
+
 
 # convert the weekend and transaction columns in numeric boolean
-dataset['Weekend'] = dataset['Weekend'].apply(converter)
-dataset['Transaction'] = dataset['Transaction'].apply(converter)
-# We can also write - dataset['OverTime'] = dataset['OverTime'].map({'Yes':1, 'No':0})
-print(dataset.info())
+#dataset['Weekend'] = dataset['Weekend'].apply(converter)
+#dataset['Transaction'] = dataset['Transaction'].apply(converter)
+    
+# we multiply by 1 to convert the boolean into 1 and 0
+dataset = dataset * 1
+
+#dataset.to_csv('test.csv')
 
 categorical_features = ['Month', 'VisitorType']
 final_data = pd.get_dummies(dataset, columns = categorical_features)
 print(final_data.info())
-print(final_data.head(2))
 
 # Dividing dataset into label and feature sets
 X = final_data.drop('Transaction', axis = 1) # Features
@@ -45,11 +46,11 @@ print(type(X))
 print(type(Y))
 print(X.shape)
 print(Y.shape)
-print(Y.head(1000))
 
 # Normalizing numerical features so that each feature has mean 0 and variance 1
 feature_scaler = StandardScaler()
 X_scaled = feature_scaler.fit_transform(X)
+print(X_scaled)
 
 # Dividing dataset into training and test sets
 X_train, X_test, Y_train, Y_test = train_test_split( X_scaled, Y, test_size = 0.3, random_state = 100)
@@ -57,7 +58,7 @@ X_train, X_test, Y_train, Y_test = train_test_split( X_scaled, Y, test_size = 0.
 print(X_train.shape)
 print(X_test.shape)
 
-np.unique(X_train)
+#np.unique(X_train)
 
 #Implementing Oversampling to balance the dataset; SMOTE stands for Synthetic Minority Oversampling TEchnique
 print("Number of observations in each class before oversampling (training data): \n", pd.Series(Y_train).value_counts())
@@ -67,70 +68,9 @@ X_train,Y_train = smote.fit_sample(X_train,Y_train)
 
 print("Number of observations in each class after oversampling (training data): \n", pd.Series(Y_train).value_counts())
 
-# Building Classification Decision Tree Model
-'''dtree = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth = 5)
-dtree.fit(X_train, Y_train)
-featimp = pd.Series(dtree.feature_importances_, index=list(X)).sort_values(ascending=False)
-print(featimp)
-
-# Evaluating Decision Tree Model
-Y_pred = dtree.predict(X_test)
-print("Prediction Accuracy: ", metrics.accuracy_score(Y_test, Y_pred)) # Not a good idea coz imbalanced test set
-conf_mat = metrics.confusion_matrix(Y_test, Y_pred)
-plt.figure(figsize=(8,6))
-sns.heatmap(conf_mat,annot=True)
-plt.title("Confusion_matrix")
-plt.xlabel("Predicted Class")
-plt.ylabel("Actual class")
-plt.show()
-print('Confusion matrix: \n', conf_mat)
-print('TP: ', conf_mat[1,1])
-print('TN: ', conf_mat[0,0])
-print('FP: ', conf_mat[0,1])
-print('FN: ', conf_mat[1,0])
-
-# Tuning the tree size parameter 'max_depth' and implementing cross-validation using Grid Search
-classifier = tree.DecisionTreeClassifier(criterion = 'entropy')
-grid_param = {'max_depth': [2,3,4,5,10,15,20,25,30,35]}
-
-gd_sr = GridSearchCV(estimator=classifier, param_grid=grid_param, scoring='recall', cv=5)
-
-"""
-In the above GridSearchCV(), scoring parameter should be set as follows:
-scoring = 'accuracy' when you want to maximize prediction accuracy
-scoring = 'recall' when you want to minimize false negatives
-scoring = 'precision' when you want to minimize false positives
-scoring = 'f1' when you want to balance false positives and false negatives (place equal emphasis on minimizing both)
-"""
-
-gd_sr.fit(X_train, Y_train)
-
-best_parameters = gd_sr.best_params_
-print(best_parameters)
-
-best_result = gd_sr.best_score_ # Mean cross-validated score of the best_estimator
-print(best_result)'''
-
-# Building random forest model
-'''rfc = RandomForestClassifier(n_estimators=300, criterion='entropy', max_features='auto')
-rfc.fit(X_train,Y_train)
-Y_pred = rfc.predict(X_test)
-conf_mat = metrics.confusion_matrix(Y_test, Y_pred)
-plt.figure(figsize=(8,6))
-sns.heatmap(conf_mat,annot=True)
-plt.title("Confusion_matrix")
-plt.xlabel("Predicted Class")
-plt.ylabel("Actual class")
-plt.show()
-print('Confusion matrix: \n', conf_mat)
-print('TP: ', conf_mat[1,1])
-print('TN: ', conf_mat[0,0])
-print('FP: ', conf_mat[0,1])
-print('FN: ', conf_mat[1,0])'''
-
 # Tuning the random forest parameter 'n_estimators' and implementing cross-validation using Grid Search
-rfc = RandomForestClassifier(criterion='entropy', max_features='auto', random_state=1)
-grid_param = {'n_estimators': [200, 250, 300, 350, 400, 450]}
+'''rfc = RandomForestClassifier(criterion='entropy', max_features='auto', random_state=1)
+grid_param = {'n_estimators': [50, 100, 150, 200, 250, 300,350,400,450]}
 
 gd_sr = GridSearchCV(estimator=rfc, param_grid=grid_param, scoring='precision', cv=5)
 
@@ -148,10 +88,11 @@ best_parameters = gd_sr.best_params_
 print(best_parameters)
 
 best_result = gd_sr.best_score_ # Mean cross-validated score of the best_estimator
-print(best_result)
-'''
+print(best_result)'''
+
+
 # Building random forest using the tuned parameter
-rfc = RandomForestClassifier(n_estimators=400, criterion='entropy', max_features='auto', random_state=1)
+rfc = RandomForestClassifier(n_estimators=250, criterion='entropy', max_features='auto', random_state=1)
 rfc.fit(X_train,Y_train)
 featimp = pd.Series(rfc.feature_importances_, index=list(X)).sort_values(ascending=False)
 print(featimp)
@@ -171,8 +112,9 @@ print('FP: ', conf_mat[0,1])
 print('FN: ', conf_mat[1,0])
 
 # Selecting features with higher sifnificance and redefining feature set
-X = final_data[['OverTime', 'Age', 'JobSatisfaction', 'StockOptionLevel', 'MonthlyIncome']]
-
+#redefine the X with the most important features
+X = final_data[['PageValue', 'ExitRate', 'ProductRelated_Duration', 'Administrative', 'ProductRelated', 'Administrative_Duration', 'BounceRate', 'Month_Nov', 'Informational','Informational_Duration']]
+#X = final_data[['PageValue', 'ExitRate']]
 feature_scaler = StandardScaler()
 X_scaled = feature_scaler.fit_transform(X)
 
@@ -182,7 +124,7 @@ X_train, X_test, Y_train, Y_test = train_test_split( X_scaled, Y, test_size = 0.
 smote = SMOTE(random_state = 101)
 X_train,Y_train = smote.fit_sample(X_train,Y_train)
 
-rfc = RandomForestClassifier(n_estimators=400, criterion='entropy', max_features='auto', random_state=1)
+rfc = RandomForestClassifier(n_estimators=250, criterion='entropy', max_features='auto', random_state=1)
 rfc.fit(X_train,Y_train)
 
 Y_pred = rfc.predict(X_test)
@@ -200,7 +142,7 @@ print('FP: ', conf_mat[0,1])
 print('FN: ', conf_mat[1,0])
 
 #########################################################
-
+'''
 # Implementing PCA to visualize dataset
 pca = PCA(n_components = 2)
 pca.fit(X_scaled)
